3ef40939688a6b29d42c642cbce43a498d6e38c6
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-01-05 21:45:36 -0300

    removing useless import

--- api_root/views.py
+++ api_root/views.py
@@ -1,6 +1,5 @@
 from .models import Drink
 from .serializers import DrinkSerializer
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
